2dbc57cf346535ffe3d2b04a35825f2e7308aeb6
Michael Nunes <tinysquid.git@gmail.com>
2020-03-11 12:35:37 -0700

    Add specs to files

--- src/player.py
+++ src/player.py
@@ -1,2 +1,6 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+# Players should have a name and current_room attributes
+
+# Add capability to add Items to the player's inventory.
+# The inventory can also be a list of items "in" the player, similar to how Items can be in a Room.
--- src/room.py
+++ src/room.py
@@ -1,2 +1,8 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+# The room should have name and description attributes.
+
+# The room should also have n_to, s_to, e_to, and w_to attributes which point to the room in that respective direction.
+
+# The Room class should be extended with a list that holds the Items that are currently in that room.
