cb062b6517a3a9daff528cead9d202ffdb306f71
Michael Nunes <tinysquid.git@gmail.com>
2020-03-11 18:02:02 -0700

    Add logic to Room for printing item list

--- src/room.py
+++ src/room.py
@@ -2,6 +2,8 @@ import textwrap
 
 
 class Room:
+    """ Blueprint for a single dungeon room """
+
     def __init__(self, name, description, is_lit=False):
         # Basic room info
         self.name = name
@@ -18,10 +20,12 @@ class Room:
         # Items inside this room
         self.items = []
 
+        # Is this room illuminated or not?
         self.is_lit = is_lit
 
     def set_exits(self, exits):
         for key, value in exits.items():
+            # Make sure key is a valid direction
             if key not in ("n", "s", "e", "w"):
                 raise KeyError("Room directional exits can only be n, s, e, or w")
             else:
@@ -30,9 +34,27 @@ class Room:
     def get_exits(self):
         return self.exits
 
+    def set_items(self, items):
+        for item in items:
+            self.items.append(item)
+
     def __str__(self):
         if self.is_lit:
-            return f"Room: {self.name}\n{textwrap.fill(self.description, 70)}"
+            # Display normal information
+            room = f"Room: {self.name}"
+            description = textwrap.fill(self.description, 70)
+
+            # Append items in room to string
+            if len(self.items) >= 1:
+                item_list = "Items in room: - "
+
+                for item in self.items:
+                    item_list += item.name + " - "
+
+                return f"{room}\n{description}\n{item_list}"
+
+            return f"{room}\n{description}"
         else:
+            # Room is dark, so you won't be able to see any items or exits!
             return "It's pitch black in here!"
 
