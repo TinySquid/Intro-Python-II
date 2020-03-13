0b8d43fc6f75283f0a5ac25d255629e211cb75d2
Michael Nunes <tinysquid.git@gmail.com>
2020-03-12 18:07:59 -0700

    Update Room to use Inventory, refactor print string format

--- src/room.py
+++ src/room.py
@@ -1,10 +1,11 @@
 import textwrap
+from inventory import Inventory
 
 
 class Room:
     """ Blueprint for a single dungeon room """
 
-    def __init__(self, name, description, is_lit=False):
+    def __init__(self, name, description, is_lit=True, loot=[]):
         # Basic room info
         self.name = name
         self.description = description
@@ -18,12 +19,14 @@ class Room:
         }
 
         # Items inside this room
-        self.items = []
+        self.inventory = Inventory(loot)
 
         # Is this room illuminated or not?
         self.is_lit = is_lit
 
     def set_exits(self, exits):
+        """ Sets a number of exits for a room """
+
         for key, value in exits.items():
             # Make sure key is a valid direction
             if key not in ("n", "s", "e", "w"):
@@ -32,28 +35,45 @@ class Room:
                 self.exits[key] = value
 
     def get_exits(self):
+        """ Returns a dict of room exits """
         return self.exits
 
-    def set_items(self, items):
-        for item in items:
-            self.items.append(item)
+    def set_loot(self, loot):
+        """ Adds a list of items into a room """
+        self.inventory.set(loot)
+
+    def get_loot(self):
+        """ Returns contents of a room's inventory """
+        return self.inventory.get()
+
+    # This allows for custom handling of certain items being dropped in a room
+    def deposit_loot_item(self, item):
+        """ Returns an item to a room's inventory """
+        self.inventory.add(item)
+
+    # This allows for custom handling of certain items being taken from a room
+    def withdraw_loot_item(self, item):
+        """ Removes an item from a room's inventory """
+        self.inventory.remove(item)
 
     def __str__(self):
         if self.is_lit:
             # Display normal information
-            room = f"Room: {self.name}"
-            description = textwrap.fill(self.description, 70)
+            pretty_string = f"Room: {self.name}\n"
+            pretty_string += textwrap.fill(self.description, 70) + "\n"
 
             # Append items in room to string
-            if len(self.items) >= 1:
-                item_list = "Items in room: - "
+            if self.inventory:
+                item_list = "\n| Loot in room |"
 
-                for item in self.items:
-                    item_list += item.name + " - "
+                for item in self.inventory.get():
+                    item_list += f"\n{item.name} : {item.description}"
 
-                return f"{room}\n{description}\n{item_list}"
+                pretty_string += item_list
 
-            return f"{room}\n{description}"
+                return f"{pretty_string}"
+            else:
+                return f"{pretty_string}"
         else:
             # Room is dark, so you won't be able to see any items or exits!
             return "It's pitch black in here!"
